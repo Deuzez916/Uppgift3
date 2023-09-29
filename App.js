6f703c5e3b7cbb4e6459526dc330c6d22430aeff
Filip Nicklasson <filip@nicklasson.nu>
2023-09-29 08:40:08 +0200

    Skapat en navigering från app.js till Welcome.js

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './Welcome';
 
 export default function App() {
+  
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Start' component={Welcome}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+// * Lägg till navigering i projektet
+// * Gör en statisk FlatList i StartVy med tre rader "Apelsinjuice", "Banansmoothie", "Cider"
+// * Gör en vy för en rad. Blå bakgrund med vit text.
+// * Klick på rad öppnar ny vy med navigering som visar texten större 
+// * Ladda upp projektet till github och ange länk nedan
